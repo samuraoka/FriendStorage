705b6f43bbc193631fa16aa1dc7b7af7d74a09b7
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-06 14:36:23 +0900

    Implemented the Friend class.

---
+++ FriendStorage.Model.Test/FriendTestShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace FriendStorage.Model.Test
+{
+    public class FriendTestShould
+    {
+        private readonly Type _type;
+
+        public FriendTestShould()
+        {
+            // Arrange
+            _type = typeof(Friend);
+        }
+
+        [Theory]
+        [InlineData("Id", typeof(int))]
+        [InlineData("FirstName", typeof(string))]
+        [InlineData("LastName", typeof(string))]
+        [InlineData("Birthday", typeof(DateTime?))]
+        [InlineData("IsDeveloper", typeof(bool))]
+        public void AccessReadWriteProperty(string propName, Type propType)
+        {
+            // Act
+            var propInfo = _type.GetProperty(propName);
+
+            // Assert
+            Assert.NotNull(propInfo);
+            Assert.Equal(propType, propInfo.PropertyType);
+            Assert.True(propInfo.CanRead);
+            Assert.True(propInfo.CanWrite);
+        }
+    }
+}
